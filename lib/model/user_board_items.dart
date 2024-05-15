7ad7ead8b2a3d5653e1f95109f12a878288137a3
Andrij <andrij.felenko@tuta.io>
2024-05-15 18:23:12 +0300

    fill first step on classes

--- lib/model/user_board_items.dart
+++ lib/model/user_board_items.dart
@@ -1,3 +1,20 @@
+import 'fishka.dart';
+import 'position.dart';
+
 class UserBoardItems {
-  //
+  UserBoardItems();
+
+  List <Fishka> _fishks;
+
+  void turn(Position from, Position to) {
+    //
+  }
+
+  bool findFishka(Position coord) {
+    for (Fishka it in _fishks) {
+      if (it.coord == coord)
+        return true;
+    }
+    return false;
+  }
 }
